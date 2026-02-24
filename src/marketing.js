b16de05669cfe4cc3e4395c2b2543a1cc924417d
Alishayakmal1 <aakmal1212@gmail.com>
2026-02-24 07:58:16 -0500

    Update marketing.js

--- src/marketing.js
+++ src/marketing.js
@@ -1,13 +1,3 @@
-import { renderSampleInsightsSection } from "./components/marketing/sampleInsightsSection.js";
-import { renderMiniDashboardPreview } from "./components/marketing/miniDashboardPreview.js";
+import { renderSampleDashboard } from "./marketingSampleDashboard.js";
 
-const insightsRoot = document.getElementById("sampleInsightsRoot");
-const miniDashboardRoot = document.getElementById("miniDashboardRoot");
-
-if (insightsRoot) {
-  renderSampleInsightsSection(insightsRoot);
-}
-
-if (miniDashboardRoot) {
-  renderMiniDashboardPreview(miniDashboardRoot);
-}
+renderSampleDashboard();
