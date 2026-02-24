ad53046b547e42ead94ceec8700c3f3a3d674ac1
Alishayakmal1 <aakmal1212@gmail.com>
2026-02-24 06:48:36 -0500

    Create sampleMiniDashboard.js

---
+++ src/data/sampleMiniDashboard.js
@@ -0,0 +1,27 @@
+/**
+ * @typedef {Object} MiniPoint
+ * @property {number} x
+ * @property {number} y
+ */
+
+export const miniKpis = [
+  { label: "Revenue", value: "$1.4M", delta: "+12%" },
+  { label: "Retention", value: "52%", delta: "+3 pp" },
+  { label: "Active users", value: "84k", delta: "+9%" },
+];
+
+/** @type {MiniPoint[]} */
+export const miniTrend = [
+  { x: 1, y: 42 },
+  { x: 2, y: 46 },
+  { x: 3, y: 44 },
+  { x: 4, y: 50 },
+  { x: 5, y: 55 },
+  { x: 6, y: 53 },
+  { x: 7, y: 58 },
+  { x: 8, y: 61 },
+  { x: 9, y: 60 },
+  { x: 10, y: 66 },
+  { x: 11, y: 69 },
+  { x: 12, y: 72 },
+];
